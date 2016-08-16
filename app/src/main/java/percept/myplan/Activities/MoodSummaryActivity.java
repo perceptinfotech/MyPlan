67609cdcabfd37b848bedb3919224629d7652506
prakashpatel <perceptinfotech@gmail.com>
2016-08-16 15:44:51 +0530

    Code for Mood Summary Heading

--- app/src/main/java/percept/myplan/Activities/MoodSummaryActivity.java
+++ app/src/main/java/percept/myplan/Activities/MoodSummaryActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -26,6 +27,7 @@ import java.util.Map;
 
 import percept.myplan.Global.Constant;
 import percept.myplan.Global.General;
+import percept.myplan.Global.Utils;
 import percept.myplan.Interfaces.VolleyResponseListener;
 import percept.myplan.POJO.Mood;
 import percept.myplan.R;
@@ -37,6 +39,7 @@ public class MoodSummaryActivity extends AppCompatActivity {
     private List<Mood> LIST_MOOD;
     Map<String, String> params;
     MoodSummaryAdapter ADAPTER;
+    private Utils UTILS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +54,7 @@ public class MoodSummaryActivity extends AppCompatActivity {
         TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
         mTitle.setText(R.string.seenotes);
 
+        UTILS = new Utils(MoodSummaryActivity.this);
         LST_MOODSUMMARY = (RecyclerView) findViewById(R.id.lstMoodSummay);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(MoodSummaryActivity.this);
         LST_MOODSUMMARY.setLayoutManager(mLayoutManager);
@@ -83,73 +87,80 @@ public class MoodSummaryActivity extends AppCompatActivity {
                             Date date = format.parse(LIST_MOOD.get(i).getMOOD_DATE());
                             Calendar cal = Calendar.getInstance();
                             cal.setTime(date);
-                            int _day = cal.get(Calendar.DAY_OF_MONTH);
-                            int _Month = cal.get(Calendar.MONTH);
-                            String _strTime = "";
-                            switch (cal.get(Calendar.DAY_OF_WEEK)) {
-                                case 1:
-                                    _strTime = "Sunday ";
-                                    break;
-                                case 2:
-                                    _strTime = "Monday ";
-                                    break;
-                                case 3:
-                                    _strTime = "Tuesday ";
-                                    break;
-                                case 4:
-                                    _strTime = "Wednesday ";
-                                    break;
-                                case 5:
-                                    _strTime = "Thursday ";
-                                    break;
-                                case 6:
-                                    _strTime = "Friday ";
-                                    break;
-                                case 7:
-                                    _strTime = "Saturday ";
-                                    break;
+                            String _suffix = UTILS.getDayOfMonthSuffix(cal.get(Calendar.DAY_OF_MONTH));
 
-                            }
-                            _strTime = _strTime + String.valueOf(_day) + "th of ";
-                            switch (_Month) {
-                                case 0:
-                                    _strTime = _strTime + "January";
-                                    break;
-                                case 1:
-                                    _strTime = _strTime + "February";
-                                    break;
-                                case 2:
-                                    _strTime = _strTime + "March";
-                                    break;
-                                case 3:
-                                    _strTime = _strTime + "April";
-                                    break;
-                                case 4:
-                                    _strTime = _strTime + "May";
-                                    break;
-                                case 5:
-                                    _strTime = _strTime + "June";
-                                    break;
-                                case 6:
-                                    _strTime = _strTime + "July";
-                                    break;
-                                case 7:
-                                    _strTime = _strTime + "August";
-                                    break;
-                                case 8:
-                                    _strTime = _strTime + "September";
-                                    break;
-                                case 9:
-                                    _strTime = _strTime + "October";
-                                    break;
-                                case 10:
-                                    _strTime = _strTime + "November";
-                                    break;
-                                case 11:
-                                    _strTime = _strTime + "December";
-                                    break;
-                            }
-                            LIST_MOOD.get(i).setMOOD_DATE_STRING(_strTime);
+                            SimpleDateFormat _getDay = new SimpleDateFormat("EEEE d'" + _suffix + "' ' of ' MMMM");
+
+                            Log.d(":::: ", _getDay.format(date));
+
+//                            int _day = cal.get(Calendar.DAY_OF_MONTH);
+//                            int _Month = cal.get(Calendar.MONTH);
+//                            String _strTime = "";
+//                            switch (cal.get(Calendar.DAY_OF_WEEK)) {
+//                                case 1:
+//                                    _strTime = "Sunday ";
+//                                    break;
+//                                case 2:
+//                                    _strTime = "Monday ";
+//                                    break;
+//                                case 3:
+//                                    _strTime = "Tuesday ";
+//                                    break;
+//                                case 4:
+//                                    _strTime = "Wednesday ";
+//                                    break;
+//                                case 5:
+//                                    _strTime = "Thursday ";
+//                                    break;
+//                                case 6:
+//                                    _strTime = "Friday ";
+//                                    break;
+//                                case 7:
+//                                    _strTime = "Saturday ";
+//                                    break;
+//
+//                            }
+//                            _strTime = _strTime + String.valueOf(_day) + "th of ";
+//                            switch (_Month) {
+//                                case 0:
+//                                    _strTime = _strTime + "January";
+//                                    break;
+//                                case 1:
+//                                    _strTime = _strTime + "February";
+//                                    break;
+//                                case 2:
+//                                    _strTime = _strTime + "March";
+//                                    break;
+//                                case 3:
+//                                    _strTime = _strTime + "April";
+//                                    break;
+//                                case 4:
+//                                    _strTime = _strTime + "May";
+//                                    break;
+//                                case 5:
+//                                    _strTime = _strTime + "June";
+//                                    break;
+//                                case 6:
+//                                    _strTime = _strTime + "July";
+//                                    break;
+//                                case 7:
+//                                    _strTime = _strTime + "August";
+//                                    break;
+//                                case 8:
+//                                    _strTime = _strTime + "September";
+//                                    break;
+//                                case 9:
+//                                    _strTime = _strTime + "October";
+//                                    break;
+//                                case 10:
+//                                    _strTime = _strTime + "November";
+//                                    break;
+//                                case 11:
+//                                    _strTime = _strTime + "December";
+//                                    break;
+//                            }
+
+                            LIST_MOOD.get(i).setMOOD_DATE_STRING(_getDay.format(date));
                         } catch (ParseException e) {
                             e.printStackTrace();
                         }
--- app/src/main/java/percept/myplan/Global/Utils.java
+++ app/src/main/java/percept/myplan/Global/Utils.java
@@ -1,12 +1,11 @@
 /**
  * @author Vivek Shah
- * 
+ * <p>
  * This class is made as common or utility class where the all common methods and
  * also common variables are stored which can be used in whole development time.
- *  
- * You should declare this class  in every activity to use common things like alert and toast even , 
+ * <p>
+ * You should declare this class  in every activity to use common things like alert and toast even ,
  * just need to pass context to constructor from there.
- * 
  */
 package percept.myplan.Global;
 
@@ -33,276 +32,295 @@ import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
+import static com.google.android.exoplayer.util.Assertions.checkArgument;
 
 
 public class Utils extends Application {
 
-	// This variable store the context of Application
-	public static Context applicationContext;
+    // This variable store the context of Application
+    public static Context applicationContext;
 
-	// private context made because no one can use ot store in this context
-	private Context context = null;
-	private String LOG_TAG = "MYPLAN_LOG";
-	private SharedPreferences preferences = null;
-	private SharedPreferences.Editor editor = null;
+    // private context made because no one can use ot store in this context
+    private Context context = null;
+    private String LOG_TAG = "MYPLAN_LOG";
+    private SharedPreferences preferences = null;
+    private SharedPreferences.Editor editor = null;
 
-	/*
-	 * Parameterized Constructor made because getting fresh context every time
-	 * and to make methods easy.
-	 */
-	public Utils(Context con) {
-		context = con;
-		preferences = PreferenceManager.getDefaultSharedPreferences(con);
-		editor = preferences.edit();
-		// LOG_TAG = context.getPackageName().toString();
-	}
+    /*
+     * Parameterized Constructor made because getting fresh context every time
+     * and to make methods easy.
+     */
+    public Utils(Context con) {
+        context = con;
+        preferences = PreferenceManager.getDefaultSharedPreferences(con);
+        editor = preferences.edit();
+        // LOG_TAG = context.getPackageName().toString();
+    }
 
-	/**
-	 * @param message
-	 *            Pass message to show user
-	 * @return It will return long toast message whatever you pass in your
-	 *         application
-	 */
-	public void Toast(String message) {
-		final String onTimeMsg = message;
-		((Activity) context).runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				Toast.makeText(context, onTimeMsg, Toast.LENGTH_SHORT).show();
-			}
-		});
-	}
+    /**
+     * @param message
+     *            Pass message to show user
+     * @return It will return long toast message whatever you pass in your
+     *         application
+     */
+    public void Toast(String message) {
+        final String onTimeMsg = message;
+        ((Activity) context).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, onTimeMsg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
-	/**
-	 * @param buttonName
-	 *            set yes no or cancel
-	 * @param message
-	 *            message in alert box
-	 * 
-	 * @return AlertBox to use as user message
-	 */
-	public void showAlertMessage(String buttonName, String message) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		builder.setMessage(message);
-		builder.setPositiveButton(buttonName,
-				new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.dismiss();
-					}
-				}).show();
-	}
+    /**
+     * @param buttonName
+     *            set yes no or cancel
+     * @param message
+     *            message in alert box
+     *
+     * @return AlertBox to use as user message
+     */
+    public void showAlertMessage(String buttonName, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage(message);
+        builder.setPositiveButton(buttonName,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
+    }
 
-	/**
-	 * @param emailAddress
-	 *            Passyour emiailaddress string to check
-	 * @return It will return true if email address is valid or false in case
-	 *         email is not valid
-	 */
-	public boolean isEmailValid(String emailAddress) {
-		String regExpn = "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
-				+ "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-				+ "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
-				+ "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-				+ "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
-				+ "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
+    /**
+     * @param emailAddress
+     *            Passyour emiailaddress string to check
+     * @return It will return true if email address is valid or false in case
+     *         email is not valid
+     */
+    public boolean isEmailValid(String emailAddress) {
+        String regExpn = "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
 
-		Pattern pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
-		Matcher matcher = pattern.matcher(emailAddress);
+        Pattern pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(emailAddress);
 
-		if (matcher.matches())
-			return true;
-		else
-			return false;
-	}
-
-	/**
-	 * @return It will check your Internet connection.True if any net connected.
-	 */
-	public boolean isNetConnected() {
-		ConnectivityManager connectivity = (ConnectivityManager) context
-				.getSystemService(Context.CONNECTIVITY_SERVICE);
-		if (connectivity != null) {
-			NetworkInfo[] info = connectivity.getAllNetworkInfo();
-			if (info != null)
-				for (int i = 0; i < info.length; i++)
-					if (info[i].getState() == NetworkInfo.State.CONNECTED) {
-						return true;
-					}
-
-		}
-		return false;
-	}
-
-	/**
-	 * @return This method returns system current time , change format as per
-	 *         your reuirement , Locale is also set as english so take care of
-	 *         that also.It is HH:mm 24 hour format
-	 */
-	public String getCurrentTime() {
-		return new SimpleDateFormat("HH:mm:ss", Locale.ENGLISH)
-				.format(new Date());
-	}
-
-	public String getCurrentTimePath() {
-		return new SimpleDateFormat("HHmmss", Locale.ENGLISH)
-				.format(new Date());
-	}
-
-	/**
-	 * @return This method returns system current date in dd/MM/yyyy format
-	 */
-	public String getCurrentDate() {
-		return new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH)
-				.format(new Date());
-	}
-
-	/**
-	 * set string Preference
-	 */
-	public void setPreference(String key, String value) {
-		editor.putString(key, value);
-		editor.commit();
-	}
-
-	/**
-	 * Use to save string preferences
-	 */
-	public void setScriptPrefrences(String key, String msg) {
-		editor.putString(key, msg);
-		editor.commit();
-
-	}
-
-	/**
-	 * Use to get string preference
-	 */
-	public String getScriptPrefrences(String key) {
-		return preferences.getString(key, "");
-	}
-
-	/**
-	 * get string preferences
-	 */
-	public String getPreference(String key) {
-		return preferences.getString(key, "");
-	}
-
-	/**
-	 * Use to set boolean preference
-	 */
-	public void setBoolPrefrences(String key, boolean value) {
-		editor.putBoolean(key, value);
-		editor.commit();
-	}
-
-	/**
-	 * use to get boolean preference
-	 */
-	public boolean getBoolPref(String key) {
-		return preferences.getBoolean(key, false);
-	}
-
-	/**
-	 * @return This method kills all processes of application which is running
-	 *         in back ground , we can also use
-	 *         android.os.Process.killProcess(pid) to exit from application
-	 */
-	public void killProcess(Context context) {
-		int pid = 0;
-		ActivityManager am = (ActivityManager) context
-				.getSystemService(Context.ACTIVITY_SERVICE);
-		List<ActivityManager.RunningAppProcessInfo> pids = am
-				.getRunningAppProcesses();
-		for (int i = 0; i < pids.size(); i++) {
-			ActivityManager.RunningAppProcessInfo info = pids.get(i);
-			if (info.processName.equalsIgnoreCase("com.dbb.activity")) {
-				pid = info.pid;
-			}
-		}
-		android.os.Process.killProcess(pid);
-	}
-
-	/**
-	 * @return true is tablet resolution is there
-	 */
-	public boolean isTablet() {
-		boolean xlarge = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == 4);
-		boolean large = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE);
-		return (xlarge || large);
-	}
-
-	/* All logs */
-	public void log(String message) {
-		Log.d(LOG_TAG, message);
-	}
-
-	public void infoLog(String message) {
-		Log.i(LOG_TAG, message);
-	}
-
-	public void errorLog(String message) {
-		Log.e(LOG_TAG, message);
-	}
-
-	public void verboseLog(String message) {
-		Log.v(LOG_TAG, message);
-	}
-
-	public void warnLog(String message) {
-		Log.w(LOG_TAG, message);
-	}
-
-	public boolean isUsingMobileData() {
-		ConnectivityManager connectivity = (ConnectivityManager) context
-				.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-		NetworkInfo mobileInfo = connectivity
-				.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-
-		if (mobileInfo.getState() == NetworkInfo.State.CONNECTED
-				|| mobileInfo.getState() == NetworkInfo.State.CONNECTING) {
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * @return IMEI NO
-	 */
-	public String getImeiNo() {
-		TelephonyManager teleManager = (TelephonyManager) context
-				.getSystemService(Context.TELEPHONY_SERVICE);
-		return teleManager.getDeviceId();
-	}
-
-	public boolean isUsingWiFi() {
-		ConnectivityManager connectivity = (ConnectivityManager) context
-				.getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo wifiInfo = connectivity
-				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-		if (wifiInfo.getState() == NetworkInfo.State.CONNECTED
-				|| wifiInfo.getState() == NetworkInfo.State.CONNECTING) {
-			return true;
-		}
-
-		return false;
-	}
+        if (matcher.matches())
+            return true;
+        else
+            return false;
+    }
 
 
-	public boolean isMemorycard() {
+    public String getDayOfMonthSuffix(final int n) {
+        checkArgument(n >= 1 && n <= 31, "illegal day of month: " + n);
+        if (n >= 11 && n <= 13) {
+            return "th";
+        }
+        switch (n % 10) {
+            case 1:
+                return "st";
+            case 2:
+                return "nd";
+            case 3:
+                return "rd";
+            default:
+                return "th";
+        }
+    }
 
-		Boolean isSDPresent = android.os.Environment.getExternalStorageState()
-				.equals(android.os.Environment.MEDIA_MOUNTED);
+    /**
+     * @return It will check your Internet connection.True if any net connected.
+     */
+    public boolean isNetConnected() {
+        ConnectivityManager connectivity = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
 
-		if (isSDPresent) {
-			// yes SD-card is present
-			return true;
-		} else {
-			// Sorry
-			return false;
-		}
+        }
+        return false;
+    }
 
-	}
+    /**
+     * @return This method returns system current time , change format as per
+     *         your reuirement , Locale is also set as english so take care of
+     *         that also.It is HH:mm 24 hour format
+     */
+    public String getCurrentTime() {
+        return new SimpleDateFormat("HH:mm:ss", Locale.ENGLISH)
+                .format(new Date());
+    }
+
+    public String getCurrentTimePath() {
+        return new SimpleDateFormat("HHmmss", Locale.ENGLISH)
+                .format(new Date());
+    }
+
+    /**
+     * @return This method returns system current date in dd/MM/yyyy format
+     */
+    public String getCurrentDate() {
+        return new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH)
+                .format(new Date());
+    }
+
+    /**
+     * set string Preference
+     */
+    public void setPreference(String key, String value) {
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    /**
+     * Use to save string preferences
+     */
+    public void setScriptPrefrences(String key, String msg) {
+        editor.putString(key, msg);
+        editor.commit();
+
+    }
+
+    /**
+     * Use to get string preference
+     */
+    public String getScriptPrefrences(String key) {
+        return preferences.getString(key, "");
+    }
+
+    /**
+     * get string preferences
+     */
+    public String getPreference(String key) {
+        return preferences.getString(key, "");
+    }
+
+    /**
+     * Use to set boolean preference
+     */
+    public void setBoolPrefrences(String key, boolean value) {
+        editor.putBoolean(key, value);
+        editor.commit();
+    }
+
+    /**
+     * use to get boolean preference
+     */
+    public boolean getBoolPref(String key) {
+        return preferences.getBoolean(key, false);
+    }
+
+    /**
+     * @return This method kills all processes of application which is running
+     *         in back ground , we can also use
+     *         android.os.Process.killProcess(pid) to exit from application
+     */
+    public void killProcess(Context context) {
+        int pid = 0;
+        ActivityManager am = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> pids = am
+                .getRunningAppProcesses();
+        for (int i = 0; i < pids.size(); i++) {
+            ActivityManager.RunningAppProcessInfo info = pids.get(i);
+            if (info.processName.equalsIgnoreCase("com.dbb.activity")) {
+                pid = info.pid;
+            }
+        }
+        android.os.Process.killProcess(pid);
+    }
+
+    /**
+     * @return true is tablet resolution is there
+     */
+    public boolean isTablet() {
+        boolean xlarge = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == 4);
+        boolean large = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE);
+        return (xlarge || large);
+    }
+
+    /* All logs */
+    public void log(String message) {
+        Log.d(LOG_TAG, message);
+    }
+
+    public void infoLog(String message) {
+        Log.i(LOG_TAG, message);
+    }
+
+    public void errorLog(String message) {
+        Log.e(LOG_TAG, message);
+    }
+
+    public void verboseLog(String message) {
+        Log.v(LOG_TAG, message);
+    }
+
+    public void warnLog(String message) {
+        Log.w(LOG_TAG, message);
+    }
+
+    public boolean isUsingMobileData() {
+        ConnectivityManager connectivity = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo mobileInfo = connectivity
+                .getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if (mobileInfo.getState() == NetworkInfo.State.CONNECTED
+                || mobileInfo.getState() == NetworkInfo.State.CONNECTING) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return IMEI NO
+     */
+    public String getImeiNo() {
+        TelephonyManager teleManager = (TelephonyManager) context
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        return teleManager.getDeviceId();
+    }
+
+    public boolean isUsingWiFi() {
+        ConnectivityManager connectivity = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiInfo = connectivity
+                .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (wifiInfo.getState() == NetworkInfo.State.CONNECTED
+                || wifiInfo.getState() == NetworkInfo.State.CONNECTING) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public boolean isMemorycard() {
+
+        Boolean isSDPresent = android.os.Environment.getExternalStorageState()
+                .equals(android.os.Environment.MEDIA_MOUNTED);
+
+        if (isSDPresent) {
+            // yes SD-card is present
+            return true;
+        } else {
+            // Sorry
+            return false;
+        }
+
+    }
 
 }
